b5a3d7b8c139ed4e5ae7d19a14f8188c20e9e4ba
Alaa Alowaidi <abd94057@gmail.com>
2026-03-12 16:41:44 +0530

    Feature: Added Sobel Edge Detection

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './', // CRITICAL: This fixes the black screen by making paths relative
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
+  plugins: [react(), mkcert()],
+  server: {
+    https: true,
   }
 })
